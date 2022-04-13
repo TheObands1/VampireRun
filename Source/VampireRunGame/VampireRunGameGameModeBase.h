ac98aa2052b3c203c977cc4ff45bf85d4edfb2ac
TheObands1 <juansebastian3080@hotmail.com>
2022-04-13 12:12:23 -0500

    Made Prototype of Night and Day Functionality

---
+++ Source/VampireRunGame/VampireRunGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VampireRunGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VAMPIRERUNGAME_API AVampireRunGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
